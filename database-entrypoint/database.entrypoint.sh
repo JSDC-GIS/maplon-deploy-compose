28300b71970ab33e997136134cf3c93c6b2d655c
jimmg35 <jim60308@gmail.com>
2024-02-20 14:11:07 +0800

    Feat: update db entrypoint

--- database-entrypoint/database.entrypoint.sh
+++ database-entrypoint/database.entrypoint.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 export PGPASSWORD=$POSTGRES_PASSWORD;
-
 # 啟動 PostgreSQL 並創建資料庫
-psql -v ON_ERROR_STOP=1 --username "$postgres_user" --dbname "$postgres_database" <<-EOSQL
-  CREATE DATABASE $postgres_database;
-  GRANT ALL PRIVILEGES ON DATABASE $postgres_database TO $postgres_user;
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+  CREATE DATABASE $APP_DB_NAME;
+  GRANT ALL PRIVILEGES ON DATABASE $APP_DB_NAME TO $POSTGRES_USER;
 EOSQL
 
 # 連接到新建的資料庫
-psql -v ON_ERROR_STOP=1 --username "$postgres_user" --dbname "$postgres_database" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$APP_DB_NAME" <<-EOSQL
   BEGIN;
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
     CREATE EXTENSION postgis;
